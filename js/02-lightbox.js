48db3a257b18e9600e7068aaaecac20a94588cf7
Iryna Konchits <uncl95@gmail.com>
2022-08-14 13:51:48 +0300

    Add style

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// console.log(galleryItems);
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const pictureContainer = document.querySelector('.gallery');
+const pictureMarkup = createPictureCardMarkup(galleryItems);
+
+pictureContainer.insertAdjacentHTML("beforeend", pictureMarkup)
+
+function createPictureCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join("");
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 300,
+    showCounter: false
+});
